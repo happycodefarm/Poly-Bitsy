880baf9c480c900cff47bd5a4384e50989653da0
happycodefarm <farmer@happycodefarm.net>
2024-11-20 10:43:56 +0100

    progress

--- poly-bitsy.js
+++ poly-bitsy.js
@@ -1,28 +1,62 @@
 
 const gamepads = {}
-let  bitsies = {}
+let bitsies = {}
 
 function gamepadHandler(event, connected) {
-  console.log("ok")
   const gamepad = event.gamepad;
-  // Note:
-  // gamepad === navigator.getGamepads()[gamepad.index]
-
+  console.log("ok")
   if (connected) {
     gamepads[gamepad.index] = gamepad;
+    let iFrame = document.createElement("iframe")
+    iFrame.setAttribute('src', `/games/game-${gamepad.index+1}.html`)
+    iFrame.setAttribute('id', `bitsy-${gamepad.index+1}`)
+    document.getElementById("container").appendChild(iFrame)
+
+    iFrame.contentWindow.addEventListener("message", (message) => {
+      let frankstEvent = new KeyboardEvent( message.data["type"], message.data )
+      iFrame.contentWindow.document.dispatchEvent( frankstEvent )
+      console.log(frankstEvent)
+    })
   } else {
     delete gamepads[gamepad.index];
+    document.getElementById(`bitsy-${gamepad.index+1}`).remove()
   }
+
+  for (let i = 0; i < Object.keys(gamepads).length; i++) {
+    console.log(i)
+  }
+
 }
 
 function setup() {
-
   console.log("Hello Multy-Bitsy")
-
+  console.log(!!(navigator.getGamepads))
   bitsies = document.getElementsByTagName("iframe")
-  console.log( bitsies)
+
+  for (const iframe of bitsies) {
+    iframe.contentWindow.addEventListener("message", (message) => {
+    let frankstEvent = new KeyboardEvent( message.data["type"], message.data )
+    iframe.contentWindow.document.dispatchEvent( frankstEvent )
+    console.log(frankstEvent)
+  }, false)}
+
   window.addEventListener("keydown", (e) => {
-    console.log(e)
+    //console.log(e)
+    let clonedEvent = {type: e.type,key: e.key,keyCode: e.keyCode,code: e.code,which: e.which}
+
+    for (const iframe of bitsies) {
+      iframe.contentWindow.postMessage(clonedEvent, "*")
+    }
+    e.preventDefault()
+  })
+
+  window.addEventListener("keyup", (e) => {
+
+    let clonedEvent = {type: e.type,key: e.key,keyCode: e.keyCode,code: e.code,which: e.which}
+    for (const iframe of bitsies) {
+      iframe.contentWindow.postMessage(clonedEvent, "*")
+    }
+    e.preventDefault()
   })
 
   window.addEventListener("gamepadconnected", (e) => {
@@ -45,59 +79,71 @@ function setup() {
     gamepadHandler(e, false)
   })
 
-  var iframes = document.getElementsByTagName("iframe");
-  /** This function is triggered whenever there\'s an input
-  * on the page.
-  *
-  * It creates a message to post to every iframes
-  * This message is a cloned KeyboardEvent
-  **/  
-  function triggerControl(e) {  
-    let clonedEvent = { type: e.type, key: e.key, keyCode: e.keyCode, code: e.code, which: e.which };
-    for (const iframe of iframes) { 
-      iframe.contentWindow.postMessage(clonedEvent, "*");
-    } 
-  }
-
   gameLoop()
 }
 
 function gameLoop() {
+  console.log("ok")
   const gamePads = navigator.getGamepads();
   if (!gamePads) {
     return
   }
 
   gamePads.forEach((gamePad, index) => {
-    gamePad.buttons.map(e => e.pressed).forEach((isPressed, buttonIndex) => {
-      if(isPressed) {
-        console.log(`Button ${buttonIndex} is pressed`)
-      }
-    })
-    let right = gamePad.axes[1] == 1
-    let left = gamePad.axes[1] == -1
-    let up = gamePad.axes[2] == -1
-    let down = gamePad.axes[2] == 1
+
+    // gamePad.buttons.map(e => e.pressed).forEach((isPressed, buttonIndex) => {
+    //   if(isPressed) {
+    //     console.log(`Button ${buttonIndex} is pressed`)
+    //   }
+    // })
+    if (gamePad == null) return
+
+    let right = gamePad.axes[1] > 0.5
+    let left = gamePad.axes[1] < -0.5
+    let up = gamePad.axes[2] < -0.5
+    let down = gamePad.axes[2] > 0.5
+    //console.log(gamePad.axes)
 
     if (up) {
-      console.log("Up arrow key is pressed")
-      bitsies[index].dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowUp','charCode':0, 'keyCode':38, 'wich':38 }))
-    
+      console.log('up')
+      let event = {type:'keydown', key:'ArrowUp', keyCode:38, code:'ArrowUp', which:38 }
+      bitsies[index].contentWindow.postMessage(event , "*");
+
+      setTimeout(function(){ 
+        event.type = 'keyup'
+        bitsies[index].contentWindow.postMessage(event , "*");
+       }, 200)
+
     } else if (down) {
-      console.log("Down arrow key is pressed")
-      bitsies[index].dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowUp','charCode':0, 'keyCode':40, 'wich':40 }))
+      let event = {type:'keydown', key:'ArrowDown', keyCode:40, code:'ArrowDown', which:40 }
+      bitsies[index].contentWindow.postMessage(event , "*")
+
+      setTimeout(function(){ 
+        event.type = 'keyup'
+        bitsies[index].contentWindow.postMessage(event , "*");
+       }, 200)
 
     } else if (right) {
-      console.log("Right arrow key is pressed")
-      bitsies[index].dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowUp','charCode':0, 'keyCode':39, 'wich':39 }))
+      let event = {type:'keydown', key:'ArrowRight', keyCode:39, code:'ArrowRight', which:39 }
+      bitsies[index].contentWindow.postMessage(event , "*");
+
+      setTimeout(function(){ 
+        event.type = 'keyup'
+        bitsies[index].contentWindow.postMessage(event , "*");
+       }, 200)
 
     } else if (left) {
-      console.log("Left arrow key is pressed")
-      bitsies[index].dispatchEvent(new KeyboardEvent('keypress',{'key':'ArrowUp','charCode':0, 'keyCode':37, 'wich':37 }))
+      let event = {type:'keydown', key:'ArrowLeft', keyCode:37, code:'ArrowLeft', which:37 }
+      bitsies[index].contentWindow.postMessage(event , "*");
 
+      setTimeout(function(){ 
+        event.type = 'keyup'
+        bitsies[index].contentWindow.postMessage(event , "*");
+       }, 200)  
     }
     // console.log(`Left stick at (${gamePad.axes[0]}, ${gamePad.axes[1]})` );
     // console.log(`Right stick at (${gamePad.axes[2]}, ${gamePad.axes[3]})` );
   })
-  start = requestAnimationFrame(gameLoop);
+  // requestAnimationFrame(gameLoop)
+  setTimeout(gameLoop,100)
 }
